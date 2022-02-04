4c63a245d913697075d639b0dcd222080066b945
Traptricker <85963782+Traptricker@users.noreply.github.com>
2022-02-03 23:14:10 -0500

    initial setup

--- BaseCode/src/main/java/frc/robot/Constants.java
+++ BaseCode/src/main/java/frc/robot/Constants.java
@@ -7,17 +7,48 @@
 
 package frc.robot;
 
-import edu.wpi.first.wpilibj.kinematics.DifferentialDriveKinematics;
+import edu.wpi.first.wpilibj.util.Units;
 
-/**
- * The Constants class provides a convenient place for teams to hold robot-wide
- * numerical or boolean constants. This class should not be used for any other
- * purpose. All constants should be declared globally (i.e. public static). Do
- * not put anything functional in this class.
- *
- * <p>
- * It is advised to statically import this class (or one of its inner classes)
- * wherever the constants are needed, to reduce verbosity.
- */
 public final class Constants {
+
+    public static final class ModuleConstants{
+        public static double wheelDiameter = Units.inchesToMeters(4);
+        public static double driveMotorToWheel = 1 / 7.13;
+        public static double turningMotorWheelRatio = 1;
+        public static double physicalMaxSpeedMetersPerSecond = 5;
+        public static double driveEncToMeters_s = driveMotorToWheel * Math.PI * wheelDiameter;
+        public static double turnEncRotToRad = turningMotorWheelRatio * 2 * Math.PI;
+        public static double driveEncRPMToMPS = driveEncToMeters_s / 60;
+        public static double turningEncRPMToRadPerS = turnEncRotToRad / 60;
+        public static double kP = 0.5;
+        public static double kI = 0.0;
+        public static double kD = 0.0;
+        public static double wheelSpinUnitsToMetersPerSecond = 10 / 2048;
+        public static double ticksToRadians = Math.PI / 512;
+        public static double ticksToMeters = Math.PI / 512;
+    }
+
+    public static final class DrivetrainConstants{
+        public static int FL_DriveID = 0-9;
+        public static int FL_TurnID = 0-9;
+        public static boolean FL_turnReversed = false;
+        public static boolean FL_driveReversed = false;
+        
+        public static int FR_DriveID = 0-9;
+        public static int FR_TurnID = 0-9;
+        public static boolean FR_turnReversed = false;
+        public static boolean FR_driveReversed = false;
+        
+        public static int BL_DriveID = 0-9;
+        public static int BL_TurnID = 0-9;
+        public static boolean BL_turnReversed = false;
+        public static boolean BL_driveReversed = false;
+        
+        public static int BR_DriveID = 0-9;
+        public static int BR_TurnID = 0-9;
+        public static boolean BR_turnReversed = false;
+        public static boolean BR_driveReversed = false;
+
+    }
+
 }
---
+++ BaseCode/src/main/java/frc/robot/subsystems/SwerveModule.java
@@ -0,0 +1,80 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+
+import edu.wpi.first.wpilibj.geometry.Rotation2d;
+import edu.wpi.first.wpilibj.kinematics.SwerveModuleState;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.ModuleConstants;;
+
+public class SwerveModule extends SubsystemBase {
+
+  private final WPI_TalonFX driveMotor;
+  private final WPI_TalonSRX turnMotor;
+
+  /** Creates a new SwerveModule. */
+  public SwerveModule(int driveMotorID, int turnMotorID, boolean driveMotorReversed, boolean turnMotorReversed) {
+
+    // Making the motor objects
+    driveMotor = new WPI_TalonFX(driveMotorID);
+    turnMotor = new WPI_TalonSRX(turnMotorID);
+
+    // Reversing them if necessary
+    driveMotor.setInverted(driveMotorReversed);
+    turnMotor.setInverted(turnMotorReversed);
+
+    // Setting up the encoders
+    driveMotor.configSelectedFeedbackSensor(FeedbackDevice.IntegratedSensor);
+    turnMotor.configSelectedFeedbackSensor(FeedbackDevice.Analog);
+
+    // Setting up PID for turnMoter
+    turnMotor.config_kP(0, ModuleConstants.kP);
+    turnMotor.config_kI(0, ModuleConstants.kI);
+    turnMotor.config_kD(0, ModuleConstants.kD);
+
+    // TODO: Setup Absolute Encoder stuff to straighten wheels when robot boots up
+
+  }
+
+  /**
+   * Gets the angle in Radians of the current turn motor
+   */
+  public double getTurnAngle() {
+    return (turnMotor.getSelectedSensorPosition() % 1024) * ModuleConstants.ticksToRadians;
+  }
+
+  /**
+   * Gets the velocity in Radians per second for the current turn motor
+   */
+  public double getTurnVelocity() {
+    return (turnMotor.getSelectedSensorVelocity()) * ModuleConstants.ticksToRadians;
+  }
+
+  // TODO: Check if this works
+  /**
+   * Gets the velocity in Meters per second of the current drive motor
+   */
+  public double getDriveVelocity() {
+    // First gets the motor shaft units per 100ms, then multiplies by 10 to get it in units per second,
+    // next it divides it by ticks and motor shaft units to get the rps,
+    // next it multiplies that by the circumference of the wheel to get inches per second
+    // finally it converts it to meters per second
+    return ((((turnMotor.getSelectedSensorVelocity()) * 10) / (2048 * 7.13) ) * 4 * Math.PI * 0.0254);
+  }
+
+  public SwerveModuleState getState() {
+    return new SwerveModuleState(getDriveVelocity(), new Rotation2d(getTurnAngle()));
+  }
+
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
